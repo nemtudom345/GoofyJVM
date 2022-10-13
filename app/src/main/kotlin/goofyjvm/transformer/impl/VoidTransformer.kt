2caa2d5d6069c15c4eea73a89bd5994f00957c36
nemtudom345 <nemtudom345@protonmail.com>
2022-10-13 22:40:03 +0200

    Added a default constructor to java.lang.Void

--- app/src/main/kotlin/goofyjvm/transformer/impl/VoidTransformer.kt
+++ app/src/main/kotlin/goofyjvm/transformer/impl/VoidTransformer.kt
@@ -3,12 +3,19 @@ package goofyjvm.transformer.impl
 import goofyjvm.transformer.Transformer
 import org.objectweb.asm.Opcodes
 import org.objectweb.asm.tree.ClassNode
+import org.objectweb.asm.tree.MethodNode
 
 class VoidTransformer : Transformer() {
 
     override fun transform(node: ClassNode) {
         if (node.name == "java/lang/Void") {
             node.access = node.access xor Opcodes.ACC_FINAL
+
+            val ctor = MethodNode()
+            ctor.name = "<init>"
+            ctor.access = Opcodes.ACC_PUBLIC
+            ctor.desc = "()V"
+            node.methods.add(ctor)
         }
     }
 
